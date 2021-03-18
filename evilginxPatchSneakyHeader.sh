25fb1ed8e00363fa0645aeb858f933fe5dfe4625
Darren Fitzpatrick <mrdarrenm@gmail.com>
2021-03-18 12:21:56 +0000

    fixes

--- evilginxPatchSneakyHeader.sh
+++ evilginxPatchSneakyHeader.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-if [ ! -f "$1" ]; then
-    echo "The file \"$1\" does not exist!"
-    exit 1
-fi
-
-if [ ! $(basename "$1") == "http_proxy.go" ]; then
-    echo "You should be pointing me at http_proxy.go"
-    echo "... not $1"
-    exit 1
-fi
-
 # if no input arguments supplied, display usage
 if [ $# -ne 1 ]
 then
@@ -23,13 +12,24 @@ then
    exit 1
 fi
 
-echo "Ttested on Evilginx v2.4.2"
+if [ ! -f "$1" ]; then
+    echo "The file \"$1\" does not exist!"
+    exit 1
+fi
+
+if [ ! $(basename "$1") == "http_proxy.go" ]; then
+    echo "You should be pointing me at http_proxy.go"
+    echo "... not $1"
+    exit 1
+fi
+
+echo "Tested on Evilginx v2.4.2"
 echo "backing up original file to /tmp/ ... just in case!"
 cp $1 /tmp/
 echo
 
 echo "The following lines will be commented out:"
-sed -n -e 183p -e 350p -e 377,379p -e 381p -e 407p -e 562,566p -e 580p -e 1450,1456p
+sed -n -e 183p -e 350p -e 377,379p -e 381p -e 407p -e 562,566p -e 580p -e 1456,1463p $1
 echo
 
 read -p "Are you sure? " -n 1 -r
@@ -43,10 +43,10 @@ then
    sed -i '407 s/^/\/\//' $1
    sed -i '562,566 s/^/\/\//' $1
    sed -i '580 s/^/\/\//' $1
-   sed -i '1450,1456 s/^/\/\//' $1
+   sed -i '1456,1463 s/^/\/\//' $1
 
-   echo "Ok, the file now looks like:"
-   sed -n -e 183p -e 350p -e 377,379p -e 381p -e 407p -e 562,566p -e 580p -e 1450,1456p
+   echo "The lines now look like:"
+   sed -n -e 183p -e 350p -e 377,379p -e 381p -e 407p -e 562,566p -e 580p -e 1456,1463p $1
 else
    echo "Ok, cancelled, bye!"
 fi
